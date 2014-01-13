b05b3c050fc64389e8b8ed96ce7fdbad89d9ca4a
Liz Borchardt <zardra@gmail.com>
2014-01-13 12:27:59 -0800

    setting up github pages

--- view.py
+++ view.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     julian_date = julian.todays_julian_date()
-    return render_template("julian.html", date=julian_date)
+    return render_template("index.html", date=julian_date)
 
 if __name__ == "__main__":
     app.run(debug=True)
